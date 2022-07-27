e0f92a5277c7ec2be2df9c973f5e698699db2e77
eric <mrtop@live.cn>
2022-07-28 02:02:48 +0800

    判断是是否有环

---
+++ leetcode/leetcode0143.c
@@ -0,0 +1,24 @@
+//
+// Created by eric on 2022/7/28.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * @return
+ */
+
+typedef struct ListNode {
+    int val;
+    struct ListNode *next;
+} Node;
+
+bool hasCycle(struct ListNode *head) {
+
+}
+
+main() {
+
+}
