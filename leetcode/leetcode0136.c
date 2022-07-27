19a0fa1e613310796e666096a3773e08c132fb1c
eric <mrtop@live.cn>
2022-07-28 01:53:47 +0800

    单身狗问题， 找到只出现一个一次的数字

---
+++ leetcode/leetcode0136.c
@@ -0,0 +1,19 @@
+//
+// Created by eric on 2022/7/28.
+//
+
+#include <stdio.h>
+
+int singleNumber(int* nums, int numsSize){
+    for (int i = 1; i < numsSize; ++i) {
+        nums[0] ^= nums[i];
+    }
+    return  nums[0];
+}
+
+int main(){
+    int nums[] = {2,2,1};
+    int res = singleNumber(nums, sizeof(nums)/ sizeof(int ));
+    printf("res:%d \n", res);
+    return 0;
+}
